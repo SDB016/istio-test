caf82543a9fa81dffe19fe9b97c7ec8ccdca8531
SDB016 <dongbin4013@naver.com>
2023-09-13 17:14:11 +0900

    istio-server -> istio-operator로 설치

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+PROFILE="LOCAL"
+
+for arg in "$@"; do
+  if [ "$arg" == "--local" ]; then
+    PROFILE="LOCAL"
+  elif [ "$arg" == "--edu" ]; then
+    PROFILE="EDU"
+  elif [ "$arg" == "--prd" ]; then
+    PROFILE="PRD"
+  fi
+done
+
 istioctl uninstall -y --purge
 kubectl delete ns istio-system
 kubectl delete ns istio-demo
+
+case "$PROFILE" in
+"LOCAL")
+    kubectl delete ns istio-operator
+    kubectl delete ns kiali-operator
+    minikube addons disable istio-provisioner
+    minikube addons disable istio
+    ;;
+esac
