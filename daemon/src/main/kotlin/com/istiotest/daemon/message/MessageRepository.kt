fb85457313098ee9b188421e34348eb10c24c7d7
dongbin Shin <dongbin4013@naver.com>
2023-10-25 15:03:30 +0900

    디렉토리 변경

--- daemon/src/main/kotlin/com/istiotest/daemon/message/MessageRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.istiotest.daemon.message
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MessageRepository: CrudRepository<Message, Long> {
-}
